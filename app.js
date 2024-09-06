a1174e86f419320adbe717e2296ec0b8b81f2af3
Yacine <157804667+Yacne@users.noreply.github.com>
2024-09-06 22:32:24 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById("searchButton").addEventListener("click", function() {
+    document.getElementById("searchPopup").style.display = "block";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+    document.getElementById("searchPopup").style.display = "none";
+});
